7983c004ac325945815e301f2f9e302df408c887
mariobp <mariobarrpach@gmail.com>
2017-06-23 19:07:43 -0500

    merge

--- web/src/app/usuarios/route.ts
+++ web/src/app/usuarios/route.ts
@@ -1,25 +1,13 @@
 import { Routes } from '@angular/router';
 import { MenuMeta } from '../app.routing';
 import { AsistenteComponent, AsistenteEditComponent, AsistenteListComponent } from './asistente/asistente.component';
-<<<<<<< HEAD
-import { AdminComponent } from './admin/admin.component';
-import { AsistenteService } from './asistente/asistente.service';
-=======
 import { AdminComponent, EditAdminComponent, ListAdminComponent } from './admin/admin.component';
 import { AsistenteService } from './asistente/asistente.service';
 import { AdminService } from './admin/admin.service';
->>>>>>> a75b9556452a39ceaafc3ff96c271708205b741f
 
 export const UsuariosRoutes: Routes = [
     {
         path: '', children: [
-<<<<<<< HEAD
-            { path: 'admin', component: AdminComponent },
-            { path: 'asistente', component: AsistenteComponent, children: [
-                { path: '', component: AsistenteListComponent},
-                { path: ':id/edit', component: AsistenteEditComponent, resolve: { asistente: AsistenteService }}
-            ]},
-=======
             {
                 path: 'admin', component: AdminComponent, children: [
                     { path: '', component: ListAdminComponent },
@@ -32,7 +20,6 @@ export const UsuariosRoutes: Routes = [
                     { path: ':id/edit', component: AsistenteEditComponent, resolve: { asistente: AsistenteService } }
                 ]
             },
->>>>>>> a75b9556452a39ceaafc3ff96c271708205b741f
         ]
     }
 
