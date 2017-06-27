9d5426d2f3e742e7ccf66fb8396731b9cdd74c33
Yamid Pico Leal <ypicoleal@gmail.com>
2017-06-27 17:18:15 -0500

    #14 #13 correccion en parametro del servicio

--- nominax/turno/views.py
+++ nominax/turno/views.py
@@ -24,7 +24,7 @@ supra.SupraConf.body = True
 def marcar_turno(request, pk):
     empleado = recursos_h.Empleado.objects.filter(pk=pk).first()
     if empleado:
-        ultimo = models.Turno.filter(empleado__pk=empleado.pk).order_by('pk').first()
+        ultimo = models.Turno.objects.filter(empleado__pk=empleado.pk).order_by('pk').first()
         if ultimo is None or ultimo.salida is not None:
             turno = models.Turno()
             turno.entrada = timezone.now()
