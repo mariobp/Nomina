c95df15552cb4269529a140878b444733cc51346
antaresgeo <ctrujillozt@gmail.com>
2017-06-30 15:41:01 -0500

    correcion sobre selector

--- web/src/app/configuracion/general/general.component.ts
+++ web/src/app/configuracion/general/general.component.ts
@@ -7,7 +7,7 @@ import { ConfiguracionService } from './general.service';
 @Component({
     template: '<router-outlet></router-outlet>'
 })
-export class GeneralComponent {}
+export class GeneralComponent { }
 
 
 @Component({
@@ -29,41 +29,55 @@ export class EditGeneralComponent implements AfterViewInit {
 
     constructor(private _fb: FormBuilder, private _s: ConfiguracionService, private _rt: Router) {
         this.form = this._fb.group({
-          tipo_corte: [[], Validators.required],
-          primer_dia: ['', [Validators.required, Validators.min(1), Validators.max(10)]],
-          segundo_dia: ['', [Validators.required, Validators.min(1), Validators.max(10)]],
-          h_recargo_nocturno_inicio: ['', Validators.required],
-          h_recargo_nocturno_fin: ['', Validators.required],
-          h_almuerzo_inicio: ['', [Validators.required, Validators.min(1)]],
-          h_almuerzo_fin: ['', [Validators.required, Validators.min(1)]],
-          extra_diurna: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
-          extra_nocturna: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
-          extra_dominical: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
-          extra_dominical_nocturna: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
+            tipo_corte: [[], Validators.required],
+            primer_dia: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
+            segundo_dia: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
+            h_recargo_nocturno_inicio: ['', Validators.required],
+            h_recargo_nocturno_fin: ['', Validators.required],
+            h_almuerzo_inicio: ['', [Validators.required, Validators.min(1)]],
+            h_almuerzo_fin: ['', [Validators.required, Validators.min(1)]],
+            extra_diurna: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
+            extra_nocturna: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
+            extra_dominical: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
+            extra_dominical_nocturna: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
         });
-        this.columns = [ 'col1', 'col2' ];
+        this.columns = ['col1', 'col2'];
         this.renderinputs = [
-          { column: 'col1', title: 'Tipo de corte', type: 'null', name: 'tipo_corte', isSelect: true, options: [
-              { title: 'Quincenal', value: 0},
-              { title: 'Mensual', value: 1}
-          ]},
-          { column: 'col1', title: 'Primer dia de corte', type: 'number', name: 'primer_dia'},
-          { column: 'col1', title: 'Segundo dia de corte', type: 'number', name: 'segundo_dia'},
-          { column: 'col1', title: 'Hora de inicio de recargo nocturno', type: 'text', name: 'h_recargo_nocturno_inicio', class: 'timepicker'},
-          { column: 'col1', title: 'Hora de finalización de recargo nocturno', type: 'text', name: 'h_recargo_nocturno_fin', class: 'timepicker'},
-          { column: 'col1', title: 'Hora de inicio de almuerzo', type: 'text', name: 'h_almuerzo_inicio' },
-          { column: 'col1', title: 'Hora de finalización del almuerzo', type: 'text', name: 'h_almuerzo_fin'},
-          { column: 'col2', title: 'Valor hora extra diurna', type: 'number', name: 'extra_diurna'},
-          { column: 'col2', title: 'Valor hora extra nocturna', type: 'number', name: 'extra_nocturna'},
-          { column: 'col2', title: 'Valor hora extra dominical', type: 'number', name: 'extra_dominical'},
-          { column: 'col2', title: 'Valor hora extra dominical nocturna', type: 'number', name: 'extra_dominical_nocturna'},
+            {
+                column: 'col1', title: 'Tipo de corte', type: 'null', name: 'tipo_corte', isSelect: true, options: [
+                    { title: 'Quincenal', value: 0 },
+                    { title: 'Mensual', value: 1 }
+                ]
+            },
+            { column: 'col1', title: 'Primer dia de corte', type: 'number', name: 'primer_dia' },
+            { column: 'col1', title: 'Segundo dia de corte', type: 'number', name: 'segundo_dia' },
+            { column: 'col1', title: 'Hora de inicio de recargo nocturno', type: 'text', name: 'h_recargo_nocturno_inicio', class: 'timepicker' },
+            { column: 'col1', title: 'Hora de finalización de recargo nocturno', type: 'text', name: 'h_recargo_nocturno_fin', class: 'timepicker' },
+            { column: 'col1', title: 'Hora de inicio de almuerzo', type: 'text', name: 'h_almuerzo_inicio', class: 'timepicker' },
+            { column: 'col1', title: 'Hora de finalización del almuerzo', type: 'text', name: 'h_almuerzo_fin' },
+            { column: 'col2', title: 'Valor hora extra diurna', type: 'number', name: 'extra_diurna' },
+            { column: 'col2', title: 'Valor hora extra nocturna', type: 'number', name: 'extra_nocturna' },
+            { column: 'col2', title: 'Valor hora extra dominical', type: 'number', name: 'extra_dominical' },
+            { column: 'col2', title: 'Valor hora extra dominical nocturna', type: 'number', name: 'extra_dominical_nocturna' },
         ];
     }
 
     ngAfterViewInit() {
-      this._s.list({}).then(data => data.json()).then(data => {
-        console.log(data.object_list);
-        this._form.setItem(data.object_list[0]);
-      });
+        this._form.prePatchValue = value => {
+            if (!Array.isArray(value.tipo_corte)) {
+                value.tipo_corte = [value.tipo_corte];
+            }
+            return value;
+        }
+        this._form.preSave = data => {
+            data.tipo_corte = data.tipo_corte[0];
+            return data;
+        };
+        this._form.successful = data => {
+
+        }
+        this._s.list({}).then(data => data.json()).then(data => {
+            this._form.setItem(data.object_list[0]);
+        });
     }
 }
