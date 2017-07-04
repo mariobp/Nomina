37d7b069a16bce939227899be3371fdaf87c3ed9
mariobp <mariobarrpach@gmail.com>
2017-07-04 15:56:59 -0500

    cuser MIDDLEWARE_CLASSES

--- nominax/nominax/settings.py
+++ nominax/nominax/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cuser.middleware.CuserMiddleware',
 ]
 
 ROOT_URLCONF = 'nominax.urls'
