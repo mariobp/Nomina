bb852115ba09b2cf36b4b779a601896cae0ecd2c
Yamid Pico Leal <ypicoleal@gmail.com>
2017-07-06 12:27:54 -0500

    cambio en migraciones

--- nominax/nominax/settings.py
+++ nominax/nominax/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'nominax.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nomina',
+        'NAME': 'ingecol',
         'USER': 'postgres',
         'PASSWORD': 'Exile*74522547',
         'HOST': '104.236.33.228',
