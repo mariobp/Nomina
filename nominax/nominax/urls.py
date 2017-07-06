b922bf735899de1464d631e750189b6f6e902613
Yamid Pico Leal <ypicoleal@gmail.com>
2017-07-06 14:25:07 -0500

    despliegue de ui

--- nominax/nominax/urls.py
+++ nominax/nominax/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -25,6 +26,7 @@ urlpatterns = [
     url(r'^turnos/', include('turno.urls')),
     url(r'^nomina/', include('nomina.urls')),
     url(r'^configuracion/', include('configuracion.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 ]
 
 if settings.DEBUG:
