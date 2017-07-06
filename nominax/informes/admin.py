913a1190e1be76a981a2579597beb4595b2e8d83
Yamid Pico Leal <ypicoleal@gmail.com>
2017-07-06 04:05:01 -0500

    finiquito

--- nominax/informes/admin.py
+++ nominax/informes/admin.py
@@ -16,9 +16,30 @@ from django.db.models import Count
 
 class NominaResource(resources.ModelResource):
 	#ventas = fields.Field(column_name="Ventas", attribute="ventas")
+	recargos = fields.Field()
+	neto = fields.Field()
+	total = fields.Field()
+	salario_legal = fields.Field()
+
+	def dehydrate_salario_legal(self, nomina):
+		return nomina.salario_legal()
+	#end def
+
+	def dehydrate_recargos(self, nomina):
+		return nomina.recargos()
+	#end def
+
+	def dehydrate_neto(self, nomina):
+		return nomina.neto()
+	#end def
+
+	def dehydrate_total(self, nomina):
+		return nomina.total()
+	#end def
+
 	class Meta:
 		model = Nomina
-		fields = ['empleado__nombre', 'empleado__apellidos']
+		fields = ['empleado__pension__nombre','empleado__eps__nombre','empleado__nombre', 'empleado__apellidos', 'corte__fecha_fin', 'empleado__cargo__nombre', 'salario_base', 'subsidio_trasporte', 'extras', 'dominical_diurna', 'extra_dominical_diurna', 'nocturna', 'extra_nocturna', 'dominical_nocturna', 'extra_dominical_nocturna', 'recargos', 'salario_legal']
 	# end class
 #end class
 reports.register_export(Nomina, NominaResource)
