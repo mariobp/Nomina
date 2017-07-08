8fe6f25f99fcabee8f7c8344be946cb8f3542862
luismoralesp <luismiguel.mopa@gmail.com>
2017-07-07 19:49:30 -0500

    servicio de produccion

--- nominax/recursos_h/views.py
+++ nominax/recursos_h/views.py
@@ -504,9 +504,9 @@ class EmpleadoSupraList(supra.SupraListView):
     list_display = ['id', 'nombre', 'apellidos', 'cedula', 'cargo', 'fecha_nacimiento',
                     'cargo__nombre', 'pension', 'pension__nombre',
                     'eps', 'eps__nombre', 'cesantia', 'cesantia__nombre',
-                    'cajacompensacion', 'cajacompensacion__nombre']
+                    'cajacompensacion', 'cajacompensacion__nombre', 'cargo__tarifario__unidad']
     search_fields = ['nombre', 'apellidos', 'cedula']
-    list_filter = ['cargo', 'pension', 'eps', 'cesantia', 'cajacompensacion', 'id']
+    list_filter = ['cargo', 'pension', 'eps', 'cesantia', 'cajacompensacion', 'id', 'cargo__tarifario__unidad']
     search_key = 'q'
     paginate_by = 10
 
