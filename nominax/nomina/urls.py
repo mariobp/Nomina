92ff45628344e859a9eec1e74be9643f1199fbaa
luismoralesp <luismiguel.mopa@gmail.com>
2017-07-19 17:59:27 -0500

    servicios de produccion

--- nominax/nomina/urls.py
+++ nominax/nomina/urls.py
@@ -6,14 +6,22 @@ urlpatterns = [
     url(r'^corte/list/$', views.CorteSupraList.as_view(), name="corte_list"),
     url(r'^send/mail/$', views.SendMailSupraList.as_view(), name="corte_list"),
 ]
+
 """
     Descuento urls
 """
 urlpatterns = urlpatterns + [
     url(r'^descuento/form/$', views.DescuentoSupraForm.as_view(), name="descuento"),
     url(r'^descuento/list/$', views.DescuentoSupraList.as_view(), name="descuento_list"),
-    url(r'^descuento/form/(?P<pk>\d+)/$',
-        views.DescuentoSupraForm.as_view(), name="descuento_edit"),
-    url(r'^descuento/delete/(?P<pk>\d+)/$',
-        views.DescuentoSupraFormDelete.as_view(), name="descuento_delete"),
+    url(r'^descuento/form/(?P<pk>\d+)/$', views.DescuentoSupraForm.as_view(), name="descuento_edit"),
+    url(r'^descuento/delete/(?P<pk>\d+)/$', views.DescuentoSupraFormDelete.as_view(), name="descuento_delete"),
+]
+"""
+    DescuentoProduccion urls
+"""
+urlpatterns = urlpatterns + [
+    url(r'^descuento/produccion/form/$', views.DescuentoProduccionSupraForm.as_view(), name="descuento_produccion"),
+    url(r'^descuento/produccion/list/$', views.DescuentoProduccionSupraList.as_view(), name="descuento_produccion_list"),
+    url(r'^descuento/produccion/form/(?P<pk>\d+)/$', views.DescuentoProduccionSupraForm.as_view(), name="descuento_produccion_edit"),
+    url(r'^descuento/produccion/delete/(?P<pk>\d+)/$', views.DescuentoProduccionSupraFormDelete.as_view(), name="descuento_produccion_delete"),
 ]
