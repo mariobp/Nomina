7d5ed9ee682fd44e72d68f0ce13634601090b3f6
mariobp <mariobarrpach@gmail.com>
2017-07-21 15:41:37 -0500

    despliegue

--- web/dist/3.chunk.js
+++ web/dist/3.chunk.js
@@ -1,6 +1,6 @@
 webpackJsonp([3],{
 
-/***/ "../../../../../src/app/usuarios/usuarios.module.ts":
+/***/ "../../../../../src/app/configuracion/configuracion.module.ts":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -12,12 +12,20 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_angular_date_value_accessor__ = __webpack_require__("../../../../angular-date-value-accessor/index.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_angular_date_value_accessor___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_angular_date_value_accessor__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__lib_lib_module__ = __webpack_require__("../../../../../src/lib/lib.module.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__route__ = __webpack_require__("../../../../../src/app/usuarios/route.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__asistente_asistente_service__ = __webpack_require__("../../../../../src/app/usuarios/asistente/asistente.service.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__asistente_asistente_component__ = __webpack_require__("../../../../../src/app/usuarios/asistente/asistente.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__admin_admin_component__ = __webpack_require__("../../../../../src/app/usuarios/admin/admin.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__admin_admin_service__ = __webpack_require__("../../../../../src/app/usuarios/admin/admin.service.ts");
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "UsuariosModule", function() { return UsuariosModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__cargo_cargo_component__ = __webpack_require__("../../../../../src/app/configuracion/cargo/cargo.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__tipocontrato_tipocontrato_component__ = __webpack_require__("../../../../../src/app/configuracion/tipocontrato/tipocontrato.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__general_general_component__ = __webpack_require__("../../../../../src/app/configuracion/general/general.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__cargo_cargo_service__ = __webpack_require__("../../../../../src/app/configuracion/cargo/cargo.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__configuracion_route__ = __webpack_require__("../../../../../src/app/configuracion/configuracion.route.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__tipocontrato_tipocontrato_service__ = __webpack_require__("../../../../../src/app/configuracion/tipocontrato/tipocontrato.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12__general_general_service__ = __webpack_require__("../../../../../src/app/configuracion/general/general.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13__tarifario_tarifario_component__ = __webpack_require__("../../../../../src/app/configuracion/tarifario/tarifario.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_14__unidadproduccion_unidadproduccion_component__ = __webpack_require__("../../../../../src/app/configuracion/unidadproduccion/unidadproduccion.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_15__unidadproduccion_unidadproduccion_service__ = __webpack_require__("../../../../../src/app/configuracion/unidadproduccion/unidadproduccion.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_16__tarifario_tarifario_service__ = __webpack_require__("../../../../../src/app/configuracion/tarifario/tarifario.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_17__banco_banco_component__ = __webpack_require__("../../../../../src/app/configuracion/banco/banco.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_18__banco_banco_service__ = __webpack_require__("../../../../../src/app/configuracion/banco/banco.service.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ConfiguracionModule", function() { return ConfiguracionModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -35,12 +43,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var UsuariosModule = (function () {
-    function UsuariosModule() {
+
+
+
+
+
+
+
+
+var ConfiguracionModule = (function () {
+    function ConfiguracionModule() {
     }
-    return UsuariosModule;
+    return ConfiguracionModule;
 }());
-UsuariosModule = __decorate([
+ConfiguracionModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
         imports: [
             __WEBPACK_IMPORTED_MODULE_2__angular_common__["a" /* CommonModule */],
@@ -48,24 +64,140 @@ UsuariosModule = __decorate([
             __WEBPACK_IMPORTED_MODULE_3__angular_forms__["FormsModule"],
             __WEBPACK_IMPORTED_MODULE_3__angular_forms__["ReactiveFormsModule"],
             __WEBPACK_IMPORTED_MODULE_4_angular_date_value_accessor__["DateValueAccessorModule"],
-            __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */].forChild(__WEBPACK_IMPORTED_MODULE_6__route__["a" /* UsuariosRoutes */])
+            __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */].forChild(__WEBPACK_IMPORTED_MODULE_10__configuracion_route__["a" /* ConfiguracionRoutes */])
         ],
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_8__asistente_asistente_component__["a" /* AsistenteComponent */],
-            __WEBPACK_IMPORTED_MODULE_8__asistente_asistente_component__["b" /* AsistenteListComponent */],
-            __WEBPACK_IMPORTED_MODULE_8__asistente_asistente_component__["c" /* AsistenteEditComponent */],
-            __WEBPACK_IMPORTED_MODULE_9__admin_admin_component__["a" /* AdminComponent */],
-            __WEBPACK_IMPORTED_MODULE_9__admin_admin_component__["b" /* EditAdminComponent */],
-            __WEBPACK_IMPORTED_MODULE_9__admin_admin_component__["c" /* ListAdminComponent */]
+            __WEBPACK_IMPORTED_MODULE_6__cargo_cargo_component__["a" /* CargoComponent */],
+            __WEBPACK_IMPORTED_MODULE_6__cargo_cargo_component__["b" /* CargoListComponent */],
+            __WEBPACK_IMPORTED_MODULE_6__cargo_cargo_component__["c" /* EditCargoComponent */],
+            __WEBPACK_IMPORTED_MODULE_7__tipocontrato_tipocontrato_component__["a" /* TipocontratoComponent */],
+            __WEBPACK_IMPORTED_MODULE_7__tipocontrato_tipocontrato_component__["b" /* TipoContratoListComponent */],
+            __WEBPACK_IMPORTED_MODULE_7__tipocontrato_tipocontrato_component__["c" /* EditTipoContratoComponent */],
+            __WEBPACK_IMPORTED_MODULE_8__general_general_component__["a" /* GeneralComponent */],
+            __WEBPACK_IMPORTED_MODULE_8__general_general_component__["b" /* EditGeneralComponent */],
+            __WEBPACK_IMPORTED_MODULE_13__tarifario_tarifario_component__["a" /* TarifarioComponent */],
+            __WEBPACK_IMPORTED_MODULE_13__tarifario_tarifario_component__["b" /* TarifarioListComponent */],
+            __WEBPACK_IMPORTED_MODULE_13__tarifario_tarifario_component__["c" /* EditTarifarioComponent */],
+            __WEBPACK_IMPORTED_MODULE_14__unidadproduccion_unidadproduccion_component__["a" /* UnidadproduccionComponent */],
+            __WEBPACK_IMPORTED_MODULE_14__unidadproduccion_unidadproduccion_component__["b" /* UnidadProduccionListComponent */],
+            __WEBPACK_IMPORTED_MODULE_14__unidadproduccion_unidadproduccion_component__["c" /* EditUnidadProduccionComponent */],
+            __WEBPACK_IMPORTED_MODULE_17__banco_banco_component__["a" /* BancoComponent */],
+            __WEBPACK_IMPORTED_MODULE_17__banco_banco_component__["b" /* EditBancoComponent */],
+            __WEBPACK_IMPORTED_MODULE_17__banco_banco_component__["c" /* BancoListComponent */]
         ],
         providers: [
-            __WEBPACK_IMPORTED_MODULE_7__asistente_asistente_service__["a" /* AsistenteService */],
-            __WEBPACK_IMPORTED_MODULE_10__admin_admin_service__["a" /* AdminService */]
+            __WEBPACK_IMPORTED_MODULE_9__cargo_cargo_service__["a" /* CargoService */],
+            __WEBPACK_IMPORTED_MODULE_11__tipocontrato_tipocontrato_service__["a" /* TipoContratoService */],
+            __WEBPACK_IMPORTED_MODULE_12__general_general_service__["a" /* ConfiguracionService */],
+            __WEBPACK_IMPORTED_MODULE_15__unidadproduccion_unidadproduccion_service__["a" /* UnidadProduccionService */],
+            __WEBPACK_IMPORTED_MODULE_16__tarifario_tarifario_service__["a" /* TarifarioService */],
+            __WEBPACK_IMPORTED_MODULE_18__banco_banco_service__["a" /* BancoService */]
         ]
     })
-], UsuariosModule);
+], ConfiguracionModule);
 
-//# sourceMappingURL=usuarios.module.js.map
+//# sourceMappingURL=configuracion.module.js.map
+
+/***/ }),
+
+/***/ "../../../../angular-date-value-accessor/date-value-accessor.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var core_1 = __webpack_require__("../../../core/@angular/core.es5.js");
+var forms_1 = __webpack_require__("../../../forms/@angular/forms.es5.js");
+exports.DATE_VALUE_ACCESSOR = {
+    provide: forms_1.NG_VALUE_ACCESSOR,
+    useExisting: core_1.forwardRef(function () { return DateValueAccessor; }),
+    multi: true
+};
+/**
+ * The accessor for writing a value and listening to changes on a date input element
+ *
+ *  ### Example
+ *  `<input type="date" name="myBirthday" ngModel useValueAsDate>`
+ */
+var DateValueAccessor = (function () {
+    function DateValueAccessor(_renderer, _elementRef) {
+        this._renderer = _renderer;
+        this._elementRef = _elementRef;
+        this.onChange = function (_) { };
+        this.onTouched = function () { };
+    }
+    DateValueAccessor.prototype.writeValue = function (value) {
+        if (!value) {
+            this._renderer.setElementProperty(this._elementRef.nativeElement, 'value', null);
+            return;
+        }
+        this._renderer.setElementProperty(this._elementRef.nativeElement, 'valueAsDate', value);
+    };
+    DateValueAccessor.prototype.registerOnChange = function (fn) { this.onChange = fn; };
+    DateValueAccessor.prototype.registerOnTouched = function (fn) { this.onTouched = fn; };
+    DateValueAccessor.prototype.setDisabledState = function (isDisabled) {
+        this._renderer.setElementProperty(this._elementRef.nativeElement, 'disabled', isDisabled);
+    };
+    DateValueAccessor.decorators = [
+        { type: core_1.Directive, args: [{
+                    // this selector changes the previous behavior silently and might break existing code
+                    // selector: 'input[type=date][formControlName],input[type=date][formControl],input[type=date][ngModel]',
+                    // this selector is an opt-in version
+                    selector: '[useValueAsDate]',
+                    providers: [exports.DATE_VALUE_ACCESSOR]
+                },] },
+    ];
+    /** @nocollapse */
+    DateValueAccessor.ctorParameters = [
+        { type: core_1.Renderer, },
+        { type: core_1.ElementRef, },
+    ];
+    DateValueAccessor.propDecorators = {
+        'onChange': [{ type: core_1.HostListener, args: ['input', ['$event.target.valueAsDate'],] },],
+        'onTouched': [{ type: core_1.HostListener, args: ['blur', [],] },],
+    };
+    return DateValueAccessor;
+}());
+exports.DateValueAccessor = DateValueAccessor;
+//# sourceMappingURL=date-value-accessor.js.map
+
+/***/ }),
+
+/***/ "../../../../angular-date-value-accessor/index.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(__webpack_require__("../../../../angular-date-value-accessor/module.js"));
+__export(__webpack_require__("../../../../angular-date-value-accessor/date-value-accessor.js"));
+//# sourceMappingURL=index.js.map
+
+/***/ }),
+
+/***/ "../../../../angular-date-value-accessor/module.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var core_1 = __webpack_require__("../../../core/@angular/core.es5.js");
+var date_value_accessor_1 = __webpack_require__("../../../../angular-date-value-accessor/date-value-accessor.js");
+var DateValueAccessorModule = (function () {
+    function DateValueAccessorModule() {
+    }
+    DateValueAccessorModule.decorators = [
+        { type: core_1.NgModule, args: [{
+                    declarations: [date_value_accessor_1.DateValueAccessor],
+                    exports: [date_value_accessor_1.DateValueAccessor]
+                },] },
+    ];
+    /** @nocollapse */
+    DateValueAccessorModule.ctorParameters = [];
+    return DateValueAccessorModule;
+}());
+exports.DateValueAccessorModule = DateValueAccessorModule;
+//# sourceMappingURL=module.js.map
 
 /***/ })
 
