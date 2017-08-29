e95b947c2693960309ef5b3897c9fab681682aa5
mariobp <mariobarrpach@gmail.com>
2017-08-29 18:11:22 -0500

    produccion

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -12,7 +12,7 @@ declare var $: any;
 export class AppComponent implements OnInit {
     title = 'angular'
     constructor(private _cs: CallService) {
-        this._cs.conf({ host: '104.236.33.228', port: '8010' });
+        // this._cs.conf({ host: '104.236.33.228', port: '8010' });
         // this._cs.conf({ host: 'isabela.com.co' });
     }
     ngOnInit() {
